d8886d7ebf5a0ed6ff0f7cf40511c551d370f7f8
fanyinqian <1697744340@qq.com>
2024-01-11 18:41:05 +0800

    fix: email类型增加可读模式

--- src/ValueType/email/FormField/index.tsx
+++ src/ValueType/email/FormField/index.tsx
@@ -1,12 +1,22 @@
-import { ProForm } from '@ant-design/pro-components';
+import {
+  ProFieldFCMode,
+  ProFieldFCRenderProps,
+  ProForm,
+} from '@ant-design/pro-components';
+import { EditOrReadOnlyContext } from '@ant-design/pro-form/es/BaseForm/EditOrReadOnlyContext';
 import { ProFormFieldItemProps } from '@ant-design/pro-form/es/typing';
 import { AutoComplete, AutoCompleteProps } from 'antd';
-import { FC, useState } from 'react';
-
-export const Email: FC<AutoCompleteProps> = () => {
+import { FC, useContext, useState } from 'react';
+import Display from '../Display';
+export const Email: FC<ProFieldFCRenderProps & AutoCompleteProps> = (props) => {
   const [options, setOptions] = useState<{ value: string; label: string }[]>(
     [],
   );
+  const { value = '-', mode } = props;
+  if (mode === 'read') {
+    return <Display text={value} />;
+  }
+  console.log('props', props.fieldProps);
 
   const handleSearch = (value: string) => {
     let res: { value: string; label: string }[] = [];
@@ -23,10 +33,10 @@ export const Email: FC<AutoCompleteProps> = () => {
 
   return (
     <AutoComplete
-      style={{ width: 200 }}
       onSearch={handleSearch}
       placeholder="input here"
       options={options}
+      {...props}
     />
   );
 };
@@ -34,9 +44,11 @@ export const Email: FC<AutoCompleteProps> = () => {
 const FormField: FC<ProFormFieldItemProps<AutoCompleteProps, any>> = (
   props,
 ) => {
+  const mode = useContext(EditOrReadOnlyContext)?.mode as ProFieldFCMode;
+  console.log('edit Mode', props);
   return (
     <ProForm.Item {...props}>
-      <Email {...props.fieldProps}></Email>
+      <Email {...props.fieldProps} mode={mode}></Email>
     </ProForm.Item>
   );
 };
--- src/ValueType/email/index.tsx
+++ src/ValueType/email/index.tsx
@@ -1,11 +1,10 @@
 import { ProRenderFieldPropsType } from '@ant-design/pro-components';
-import React from 'react';
 import Display from './Display';
 import { Email } from './FormField';
 
 const email: ProRenderFieldPropsType = {
   render: (text) => <Display text={text} />,
-  renderFormItem: (text, props) => <Email {...props.fieldProps} />,
+  renderFormItem: (text, props) => <Email {...props} />,
 };
 
 export default email;
