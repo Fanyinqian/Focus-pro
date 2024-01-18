f2205ad1faabc7a94efddf3d8a83973bb120858a
fanyinqian <1697744340@qq.com>
2024-01-18 16:21:32 +0800

    fix: 修改ProConfigProvider的属性

--- src/ProConfigProvider/index.tsx
+++ src/ProConfigProvider/index.tsx
@@ -1,14 +1,32 @@
-import { ProConfigProvider } from '@ant-design/pro-components';
+import {
+  ProConfigProvider,
+  ProRenderFieldPropsType,
+} from '@ant-design/pro-components';
+import { ConfigProvider } from 'antd';
 import React from 'react';
 import valueTypeMap from '../ValueType';
+// import theme from '../Theme';
+import type { ThemeConfig } from 'antd';
 
-const ZFProConfigProvider: typeof ProConfigProvider = (props) => {
-  const { children, ...rest } = props;
+interface ProviderConfigProps {
+  children: React.ReactNode;
+  autoClearCache?: boolean;
+  valueTypeMap?: Record<string, ProRenderFieldPropsType>;
+  hashed?: boolean;
+  dark?: boolean;
+  prefixCls?: string;
+  theme?: ThemeConfig;
+}
+
+const ZFProConfigProvider = (props: ProviderConfigProps) => {
+  const { children, theme, ...rest } = props;
 
   return (
-    <ProConfigProvider valueTypeMap={valueTypeMap} hashed={false} {...rest}>
-      {React.Children.map(children, (c) => c)}
-    </ProConfigProvider>
+    <ConfigProvider theme={theme}>
+      <ProConfigProvider valueTypeMap={valueTypeMap} hashed={false} {...rest}>
+        {React.Children.map(children, (c) => c)}
+      </ProConfigProvider>
+    </ConfigProvider>
   );
 };
 
