b4285b4541df9ef92aad1644cd69a5cfec38b25d
fanyinqian <1697744340@qq.com>
2024-01-21 23:30:14 +0800

    chore: 新增自动化部署yml文件
    parent 62a8809d8d0ba31c1455f8874f1dd2439307e92f
    author fanyinqian <1697744340@qq.com> 1705855675 +0800
    committer fanyinqian <1697744340@qq.com> 1705916830 +0800
    
    parent 62a8809d8d0ba31c1455f8874f1dd2439307e92f
    author fanyinqian <1697744340@qq.com> 1705855675 +0800
    committer fanyinqian <1697744340@qq.com> 1705916776 +0800
    
    chore: 修改部署文档
    
    fix: 合并冲突
    
    Merge branch 'master' of github.com:Fanyinqian/Forcus-pro
    
    chore: 修改dist目录
    
    chore: 修改部署文件dist
    
    chore: 修改部署文件dist
    
    chore: 修改部署文件dist
    
    chore: 修改部署文件dist
    
    chore: dumi配置文件配置base和publicPath
    
    chore: dumi配置文件配置base和publicPath
    
    chore: 修改logo路径

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'dumi';
+// github仓库名称
+const defaultPath = '/Focus-pro';
+// 打包后gh-pages默认会拼接仓库名称在路径上
+const baseUrl = process.env.NODE_ENV === 'production' ? defaultPath : '';
 
 export default defineConfig({
+  base: defaultPath,
+  publicPath: `${baseUrl}/`,
   outputPath: 'docs-dist',
-  logo: '/images/pro.png',
-  favicons: ['/images/favicon.png'],
+  logo: `${baseUrl}/images/pro.png`,
+  favicons: [`${baseUrl}/images/favicon.png`],
   themeConfig: {
     name: 'focus pro',
     prefersColor: {
